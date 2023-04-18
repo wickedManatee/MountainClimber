4476bf2502eed5e8dd0edc6e57e15f1e84b017f2
will-mearns <will.mearns@unity3d.com>
2023-04-18 09:55:49 -0500

    curvey

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour
+public class PlayerInput : NetworkBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,8 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!IsOwner) return;
+
         Vector3 moveDir = new Vector3(0, 0, 0);
 
         if (Input.GetKey(KeyCode.Q)) moveDir.x = -2f;
